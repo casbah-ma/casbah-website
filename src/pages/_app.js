1a7c78f4208de2f37f4acf2c584d1a7e68ae7115
redapy <baha.redaaaa@gmail.com>
2023-05-03 22:33:16 +0100

    add custom fonts maco example

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import GlobalStyles from "@/components/GlobalStyles";
 import "@/styles/globals.css";
+import { Mako } from "@next/font/google";
+
+const mako = Mako({
+  subsets: ["latin"],
+  variable: "--font-mako",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={`${mako.variable}`}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
