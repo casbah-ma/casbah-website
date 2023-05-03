d3471ed0e513819ced1d3ef6f9c0ae180e2839bc
redapy <baha.redaaaa@gmail.com>
2023-05-03 23:07:26 +0100

    add server stylesheet and fix bug fs not found

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,34 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
+
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // https://storybook.js.org/docs/react/configure/frameworks#which-frameworks-are-supported
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  stories: [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
+  core: {
+    disableTelemetry: true,
   },
-  docs: {
-    autodocs: "tag",
+  // Twin recommends adding the babel config here as Next.js disables SWC when .babelrc is added to the root.
+  babel: async (options) => {
+    return {
+      ...options,
+      plugins: [
+        ...options.plugins,
+        "babel-plugin-twin",
+        "babel-plugin-macros",
+        "babel-plugin-styled-components",
+      ],
+    };
   },
 };
 export default config;
