ab31d7301930011239e9bcfab16ffa6112459bb4
Yassir BOLLES <ysrbolles@gmail.com>
2023-05-10 23:34:38 +0100

    add PropTypes

--- src/components/BlogsCard/index.jsx
+++ src/components/BlogsCard/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import MyImage from "../MyImage";
 import Paragraph from "../Paragraph";
 import Tag from "../Tag";
@@ -14,10 +15,11 @@ const BlogsCard = ({ variant, title, description, imgSrc, tags, ...rest }) => {
     <CardWrapper variant={variant} {...rest}>
       {tags && (
         <CardTags>
-          {tags.map((tag, index) => (
-            console.log(tag),
-            <Tag key={tag + index} title={tag} />
-          ))}
+          {tags.map(
+            (tag, index) => (
+              console.log(tag), (<Tag key={tag + index} title={tag} />)
+            )
+          )}
         </CardTags>
       )}
       <MyImage sizes={imagesSizes[variant]} src={imgSrc} alt={title} />
@@ -27,6 +29,14 @@ const BlogsCard = ({ variant, title, description, imgSrc, tags, ...rest }) => {
       </CardInfo>
     </CardWrapper>
   );
+};
+
+BlogsCard.propTypes = {
+  variant: PropTypes.oneOf(["v1", "v2"]),
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default BlogsCard;
