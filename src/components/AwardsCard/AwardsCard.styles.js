e706fad63a463ab6b3fdab73d047f932a4a54bed
Yassir BOLLES <ysrbolles@gmail.com>
2023-05-10 20:28:17 +0100

    finished Awards Card

--- src/components/AwardsCard/AwardsCard.styles.js
+++ src/components/AwardsCard/AwardsCard.styles.js
@@ -8,6 +8,6 @@ export const imagesSize = tw`w-[14.375rem] h-[11.375rem] object-cover bg-cover
 export const CardWrapper = tw.div`max-w-[31.813rem] flex flex-col justify-start items-start gap-4 md:gap-6`;
 
 
-export const CardInfo = tw.div`w-full flex flex-col gap-4`
+export const CardInfo = tw.div`w-full flex flex-col justify-between items-start gap-2`
 export const CardTitle = tw.h3`font-made text-lg md:text-2xl font-light text-darkBlue uppercase`;
 export const CardText = tw.p`font-made text-base font-light text-darkBlue uppercase`;
