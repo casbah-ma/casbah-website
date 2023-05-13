72e480f8fe5883cceb2312ed4b111e640fe6ed40
Yassir BOLLES <ysrbolles@gmail.com>
2023-05-13 13:47:23 +0100

    Handle input Component

--- src/components/Input/Input.styles.js
+++ src/components/Input/Input.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
-export const InputWrapper = tw.div`w-full flex flex-col gap-2`;
+export const InputWrapper = tw.div`w-full flex flex-col`;
 
 export const InputLabel = tw.label`text-base text-darkBlue text-opacity-50 font-normal font-inter capitalize`;
 
