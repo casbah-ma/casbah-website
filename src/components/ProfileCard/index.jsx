2b0016655c2a764a6ef91857e5e05df6de1c466d
redapy <baha.redaaaa@gmail.com>
2023-05-16 22:44:46 +0100

    fix the ProfileCard styles to adjust with the grid

--- src/components/ProfileCard/index.jsx
+++ src/components/ProfileCard/index.jsx
@@ -1,16 +1,29 @@
-import { CardInfo, CardText, CardTitle, CardWrapper, imagesSize } from "./ProfileCard.styles"
-import MyImage from '../MyImage'
+import PropTypes from "prop-types";
+import {
+  CardInfo,
+  CardText,
+  CardTitle,
+  CardWrapper,
+  imagesSize,
+} from "./ProfileCard.styles";
+import MyImage from "../MyImage";
 
-const ProfileCard = ({name, post, imgSrc}) => {
-    return (
-        <CardWrapper>
-            <MyImage src={imgSrc} alt={name} sizes={imagesSize} />
-            <CardInfo>
-                <CardTitle>{name}</CardTitle>
-                <CardText>{post}</CardText>
-            </CardInfo>
-        </CardWrapper>
-    )
-}
+const ProfileCard = ({ name, post, imgSrc }) => {
+  return (
+    <CardWrapper>
+      <MyImage src={imgSrc} alt={name} sizes={imagesSize} />
+      <CardInfo>
+        <CardTitle>{name}</CardTitle>
+        <CardText>{post}</CardText>
+      </CardInfo>
+    </CardWrapper>
+  );
+};
 
-export default ProfileCard
+ProfileCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  post: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+};
+
+export default ProfileCard;
