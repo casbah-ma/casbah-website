7bec9473360b4c37ab1ac1745a2d567661991ba6
redapy <baha.redaaaa@gmail.com>
2023-05-20 13:27:46 +0100

    better approach with after pseudo-element

--- src/components/XButton/XButton.styles.js
+++ src/components/XButton/XButton.styles.js
@@ -4,11 +4,8 @@ import tw from "twin.macro";
 export const StyledBtn = styled.button`
   ${tw`relative flex justify-center items-center gap-2 border-2 border-black text-center
      font-made text-base font-light leading-5 transition-all hover:rounded-[5rem]
+     after:(-z-10 absolute inset-0 transform scale-0 origin-bottom rounded-[5rem] bg-brightOrange duration-300)
+     hover:after:scale-100
      md:(min-w-[15.688rem])
      lg:(min-w-[19.563rem] h-[5.875rem])`};
 `;
-
-export const Bg = styled.div(({ isHovered }) => [
-  tw`-z-10 absolute inset-0 transform scale-0 origin-bottom rounded-[5rem] bg-brightOrange duration-300`,
-  isHovered && tw`scale-100`,
-]);
--- src/components/XButton/index.jsx
+++ src/components/XButton/index.jsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import { Bg, StyledBtn } from "./XButton.styles";
+import { StyledBtn } from "./XButton.styles";
 
 const XButton = ({ children, rest }) => {
-  const [isHovered, setIsHovered] = useState(false);
-  return (
-    <StyledBtn
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      {...rest}
-    >
-      {children}
-      <Bg isHovered={isHovered} />
-    </StyledBtn>
-  );
+  return <StyledBtn {...rest}>{children}</StyledBtn>;
 };
 
 export default XButton;
