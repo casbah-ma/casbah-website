b295592251e572d1c302a7d7ca8dd1c4a6229240
Yassir BOLLES <ysrbolles@gmail.com>
2023-05-22 22:39:04 +0100

    add Button to the form

--- src/components/Forms/ContactUs/ContactUs.styles.js
+++ src/components/Forms/ContactUs/ContactUs.styles.js
@@ -4,8 +4,7 @@ export const Wrapper = tw.section`w-full flex flex-col gap-14
                         md:(gap-12 [&>h1]:!text-[2.5rem])
                         lg:(gap-10 [&>h1]:!text-[3.5rem])`
 
-export const ContactForm = tw.form`w-full flex flex-col gap-8`
+export const ContactForm = tw.form`w-full flex flex-col gap-8 
+                                   [&>button]:(w-full md:w-[19.563rem] mt-3)`
 
-
-export const Button = tw.button`border-deepBlue text-deepBlue text-base font-made font-bold uppercase py-3.5 px-8 rounded-md border`
 
--- src/components/Forms/ContactUs/index.jsx
+++ src/components/Forms/ContactUs/index.jsx
@@ -1,15 +1,16 @@
-import { Button, ContactForm, Wrapper } from "./ContactUs.styles";
-import Input from "../../Input";
-import Title from "../../Title";
-import { useState } from "react";
-import useTranslation from "next-translate/useTranslation";
+import { ContactForm, Wrapper } from './ContactUs.styles';
+import Input from '../../Input';
+import Title from '../../Title';
+import { useState } from 'react';
+import useTranslation from 'next-translate/useTranslation';
+import Button from '../../Button';
 
-const ContactUs = ({ handleSubmit }) => {
+const ContactUs = ({}) => {
   const { t } = useTranslation();
   const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
+    name: '',
+    email: '',
+    message: '',
   });
 
   const handleInputChange = (e) => {
@@ -19,37 +20,44 @@ const ContactUs = ({ handleSubmit }) => {
     }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
   return (
     <Wrapper>
       <Title renderAs="h1" withoutBorder={true}>
-        {t("contactus")}
+        {t('contactus')}
       </Title>
       <ContactForm onSubmit={handleSubmit} defaultValue={formState}>
         <Input
           name="name"
           value={formState.name}
           type="text"
-          label={t("yourname")}
-          placeholder={t("typein" + " " + "yourfullname")}
+          label={t('yourname')}
+          placeholder={t('typein' + ' ' + 'yourfullname')}
           onChange={handleInputChange}
         />
         <Input
           name="email"
           value={formState.email}
           type="email"
-          label={t("youremail")}
-          placeholder={t("typein" + " " + "youremail")}
+          label={t('youremail')}
+          placeholder={t('typein' + ' ' + 'youremail')}
           onChange={handleInputChange}
         />
         <Input
           name="message"
           value={formState.message}
           type="text"
-          label={"yourmessage"}
-          placeholder={t("typein" + " " + "yourmessage")}
+          label={'yourmessage'}
+          placeholder={t('typein' + ' ' + 'yourmessage')}
           onChange={handleInputChange}
         />
-        <Button type="submit">{t("send")}</Button>
+        <Button type="submit" variant="primary">
+          {t('send')}
+        </Button>
       </ContactForm>
     </Wrapper>
   );
