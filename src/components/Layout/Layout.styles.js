6e6512a5e0e29b698c4765a405226fa85d34d334
redapy <baha.redaaaa@gmail.com>
2023-05-27 22:10:04 +0100

    small fix

--- src/components/Layout/Layout.styles.js
+++ src/components/Layout/Layout.styles.js
@@ -2,6 +2,6 @@ import tw from 'twin.macro';
 
 export const Wrapper = tw.main`
    flex flex-col justify-center items-center pt-[5.563rem] px-4
-   md:(pt-[13.75rem] px-10)
-   lg:(pt-[8.375rem] px-[4.5rem])
+   md:(pt-[4rem] px-10)
+   lg:(px-[4.5rem])
 `;
