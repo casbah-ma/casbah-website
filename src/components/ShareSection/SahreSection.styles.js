6bcc073aff4f4df86cef72be974875fe837a39f4
Yassir BOLLES <ysrbolles@gmail.com>
2023-06-04 12:20:58 +0100

    Create SimilarBlogs Component

--- src/components/ShareSection/SahreSection.styles.js
+++ src/components/ShareSection/SahreSection.styles.js
@@ -1,3 +1,3 @@
 import tw from 'twin.macro';
 
-export const Wrapper = tw.div`w-full flex flex-col items-start justify-center gap-5 lg:(flex-row gap-20 items-center)`;
+export const Wrapper = tw.section`w-full flex flex-col items-start justify-center gap-5 lg:(flex-row gap-20 items-center)`;
---
+++ src/components/SimilarBlog/index.jsx
@@ -0,0 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Title from '../Title';
+import { SectionTitle, Wrapper } from './SimilarBlog.styles';
+import useTranslation from 'next-translate/useTranslation';
+import BlogsCard from '../BlogsCard';
+import { A11y } from 'swiper';
+
+const SimilarBlog = ({ blogs }) => {
+  const { t } = useTranslation();
+  return (
+    <Wrapper>
+      <SectionTitle>
+        <Title renderAs="h2" withoutBorder>
+          {t('similarBlogs')}
+        </Title>
+      </SectionTitle>
+      <Swiper
+        className="w-full"
+        modules={[A11y]}
+        slidesPerView={1}
+        spaceBetween={20}
+        breakpoints={{
+          768: {
+            slidesPerView: 'auto',
+          },
+        }}
+      >
+        {blogs?.map((item, i) => (
+          <SwiperSlide
+            className="w-fit md:w-[31.813rem] h-fit"
+            key={item.name + i}
+          >
+            <BlogsCard {...item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Wrapper>
+  );
+};
+
+export default SimilarBlog;
