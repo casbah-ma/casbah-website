16f95f4d47d1209e13d2cf11d5f4f6792f127938
redapy <baha.redaaaa@gmail.com>
2023-07-12 14:43:20 +0100

    fix spacing between section for the expertise pages

--- src/pages/formulate/index.js
+++ src/pages/formulate/index.js
@@ -20,7 +20,7 @@ export const getStaticProps = ({ locale }) => {
 
 function Formulate({ data }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-[120px] mb-36">
+    <div className="flex flex-col justify-center items-center gap-20 mb-36">
       <LottieText lottie={formulate} />
       <Header {...data.headerProps} isVertical={true} />
       <PicThreeLeft
