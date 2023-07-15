37834dc1fced396756a8544b03187df70fe958e3
redapy <baha.redaaaa@gmail.com>
2023-07-15 18:14:28 +0100

    fix the CursorTracker position

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -2,6 +2,7 @@ import PortfolioHeader from '@/components/PortfolioHeader';
 import ProjectSection from '@/components/ProjectSection';
 import { allPortfolios, allBlogs } from 'contentlayer/generated';
 import { Container } from '../../styles/portfolio.styles';
+import CursorTracker from '../../components/CursorTracker';
 
 export const getStaticProps = ({ locale }) => {
   const data = allPortfolios.find((home) => home.lang === locale);
@@ -16,6 +17,7 @@ export const getStaticProps = ({ locale }) => {
 export default function Portfolio({ data }) {
   return (
     <Container>
+      <CursorTracker text="fullProject" />
       <PortfolioHeader {...data.headerProps} />
       {data?.blogs?.map((blog, key) => (
         <ProjectSection key={key} isFirst={key === 0} {...blog} />
