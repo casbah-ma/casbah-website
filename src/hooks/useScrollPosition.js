d8299a01b77574e903772505278c36b060759d2c
redapy <baha.redaaaa@gmail.com>
2023-08-01 16:16:53 +0100

    small fix

--- src/hooks/useScrollPosition.js
+++ src/hooks/useScrollPosition.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 const useScrollPosition = () => {
-  const [scrollPosition, setScrollPosition] = useState(0)
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const updatePosition = () => {
-      setScrollPosition(window.scrollY)
-    }
-    window.addEventListener('scroll', updatePosition)
-    updatePosition()
-    return () => window.removeEventListener('scroll', updatePosition)
-  }, [])
+      setScrollPosition(window.scrollY);
+    };
+    window.addEventListener('scroll', updatePosition);
+    updatePosition();
+    return () => window.removeEventListener('scroll', updatePosition);
+  }, []);
 
-  return scrollPosition
-}
+  return scrollPosition;
+};
 
-export default useScrollPosition
+export default useScrollPosition;
