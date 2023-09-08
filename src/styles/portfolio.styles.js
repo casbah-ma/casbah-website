ed5e37014ccac6543292a7dd9144761bde68c6f2
redapy <baha.redaaaa@gmail.com>
2023-09-08 12:26:15 +0100

    fix lab, portfolio, blogs pages spacing on mobile

--- src/styles/portfolio.styles.js
+++ src/styles/portfolio.styles.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const Container = styled(motion.div)`
-  ${tw`min-h-screen w-full flex flex-col items-center gap-20 mb-40 mt-[11.5rem]`}
+  ${tw`min-h-screen w-full flex flex-col items-center mb-24 gap-14 mt-[9.188rem] 
+      lg:(gap-20 mb-40 mt-[11.5rem])`}
 `;
