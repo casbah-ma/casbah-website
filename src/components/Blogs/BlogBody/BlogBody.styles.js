00c37d960ff837dd12a7ccd43f154be8ffd6bc14
Yassir BOLLES <ysrbolles@gmail.com>
2023-12-27 12:10:05 +0100

    Fix styling issue in BlogBody.styles.js and remove unnecessary className in RightArrow.jsx

--- src/components/Blogs/BlogBody/BlogBody.styles.js
+++ src/components/Blogs/BlogBody/BlogBody.styles.js
@@ -3,7 +3,7 @@ import tw from 'twin.macro';
 export const BodyWrapper = tw.section`w-full flex flex-col gap-12
                             [&>h1]:(font-made font-light text-xl md:text-[2rem] w-full lg:max-w-[54.688rem])
                             [&>blockquote]:(w-full bg-gray py-8 px-9 border-l border-solid italic border-black md:max-w-[38.188rem] lg:(max-w-[51.813rem])  self-end gap-5 [&>div]:mt-10)
-                            [&>p]:(w-full flex flex-col justify-start items-start md:max-w-[38.188rem] lg:(max-w-[51.813rem])  self-end gap-5 [&>div]:mt-10)  
+                            [&>p]:(w-full flex flex-col justify-start items-start md:max-w-[38.188rem] lg:(max-w-[51.813rem])  self-end gap-5 [&>div]:mt-10 )  
                             [&>img]:(w-full h-[8.167rem] object-cover
                                 md:(!h-[15.079rem]) 
                                 lg:(!h-[28.294rem]))
--- src/icons/RightArrow.jsx
+++ src/icons/RightArrow.jsx
@@ -6,7 +6,6 @@ const RightArrow = ({ color }) => {
       viewBox="0 0 28 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="w-full"
     >
       <path d="M20 1L27 7L20 13" stroke="black" />
       <path d="M27 7C26.6 7 8.83333 7 0 7" stroke="black" />
